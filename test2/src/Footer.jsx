9b2e1047e3d84f42c2d3dc8202f9ffcdf52fa998
naynti <crismichaelangelomortes@gmail.com>
2024-08-21 04:45:47 -0700

    update

---
+++ test2/src/Footer.jsx
@@ -0,0 +1,9 @@
+ function Footer() {
+    return(
+        <footer>
+            <p>&copy; Your website</p>
+        </footer>
+    );
+ }
+
+ export default Footer
---
+++ test2/src/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header(){
+    return(
+        <><header>
+            <h1>My Website</h1>
+            <nav>
+                <ul><a href="#"></a>Home</ul>
+                <ul><a href="#"></a>Abou</ul>
+                <ul><a href="#"></a>Services</ul>
+                <ul><a href="#"></a>Contact</ul>
+            </nav>
+        </header><hr></hr></>
+    );
+
+}
+
+export default Header
